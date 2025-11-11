18b2482d88a3a4247ad75abf9530195c2f5168f3
Alexander Lazaris <alex.lazaris617@gmail.com>
2025-11-11 22:14:51 +1100

    feat: use config file or default value for api url

--- lib/services/saves_service.dart
+++ lib/services/saves_service.dart
@@ -3,8 +3,13 @@ import 'package:ffvii_app/models/save.dart';
 
 final dio = Dio();
 
+const String apiBaseUrl = String.fromEnvironment(
+  'apiBaseUrl',
+  defaultValue: 'http://localhost:7777',
+);
+
 Future<List<Save>> getAllSaves() async {
-  final response = await dio.get<List<dynamic>>('http://localhost:7777/saves');
+  final response = await dio.get<List<dynamic>>("$apiBaseUrl/saves");
   final data = response.data as List<dynamic>;
   print(data);
   final List<Save> saves = data
