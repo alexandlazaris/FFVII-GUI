372e6bd017e9a319e1df1a3635227cc26f9a2661
Alexander Lazaris <alex.lazaris617@gmail.com>
2025-11-11 22:14:02 +1100

    feat: store pages into own dir

---
+++ lib/pages/saves_page.dart
@@ -0,0 +1,28 @@
+import 'package:ffvii_app/widgets/save_menu/save_slot_list_window.dart';
+import 'package:ffvii_app/widgets/save_menu/save_slots_top_bar.dart';
+import 'package:flutter/material.dart';
+
+class SavesPage extends StatelessWidget {
+  const SavesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 0, 75),
+        title: Text("Saves"),
+      ),
+      body: Column(
+        children: [
+          SaveSlotsTopBar(width: _screenWidth),
+          _screenWidth >= 400
+              // TODO: desktop layout needs refactoring, mobile is good to go
+              ? SaveSlotListWindowDesktop()
+              : SaveSlotListWindowMobile(),
+        ],
+      ),
+    );
+  }
+}
